9c855bf04e85ccec162fae55ef3a480b90843806
New-Bucket <azizjonnormatov437@gmail.com>
2022-12-29 09:58:08 +0500

    like

--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-
+//.
 const LikeButton = ({ isLike, handleLike, handleUnLike }) => {
     const { theme } = useSelector(state => state);
     return (
