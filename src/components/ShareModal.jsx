4334873bd979b4b51313f49020587df407ee2a0d
New-Bucket <azizjonnormatov437@gmail.com>
2022-12-29 09:59:14 +0500

    share

--- src/components/ShareModal.jsx
+++ src/components/ShareModal.jsx
@@ -8,7 +8,7 @@ import {
     TwitterShareButton, TwitterIcon,
     WhatsappShareButton, WhatsappIcon
 } from 'react-share'
-
+//.
 const ShareModal = ({url, theme}) => {
     return (
         <div className="d-flex justify-content-between px-4 py-2" style={{filter: theme ? 'invert(1)': 'invert(0)'}}>
