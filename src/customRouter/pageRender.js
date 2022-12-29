c85f002e14635e36adf78ab5325fbd0eddb279be
New-Bucket <azizjonnormatov437@gmail.com>
2022-12-29 10:01:06 +0500

    pages

--- src/customRouter/pageRender.js
+++ src/customRouter/pageRender.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {useParams} from 'react-router-dom';
 import NotFound from '../components/NotFound';
 import { useSelector } from 'react-redux';
-
+//.
 const generatePage = (pageName) => {
     const component = () => require(`../pages/${pageName}`).default;
 
