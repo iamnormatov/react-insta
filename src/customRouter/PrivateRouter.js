e0ae9bf1989c77f1a8ecc9d509a143c52507946c
New-Bucket <azizjonnormatov437@gmail.com>
2022-12-29 10:01:15 +0500

    private

--- src/customRouter/PrivateRouter.js
+++ src/customRouter/PrivateRouter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-
+//.
 const PrivateRouter = (props) => {
     const firstLogin = localStorage.getItem('firstLogin');
     return firstLogin ? <Route {...props} /> : <Redirect to="/" />;
