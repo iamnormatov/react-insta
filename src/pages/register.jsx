4452e63fe147a344202d83ee1f8232abef568eb1
New-Bucket <azizjonnormatov437@gmail.com>
2022-12-29 10:03:29 +0500

    register

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory, Link } from 'react-router-dom';
 import { register } from '../redux/actions/authAction';
-
+//.
 
 const Register = () => {
     const { auth, alert } = useSelector(state => state);
