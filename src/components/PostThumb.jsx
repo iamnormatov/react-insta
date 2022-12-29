6c85f5f24221dc2f9b1aede548664eb9044cd6f3
New-Bucket <azizjonnormatov437@gmail.com>
2022-12-29 09:59:04 +0500

    post

--- src/components/PostThumb.jsx
+++ src/components/PostThumb.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-
+//.
 const PostThumb = ({ posts, result }) => {
     const { theme } = useSelector(state => state);
 
