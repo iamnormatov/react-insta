b138562073490cc8d901607280ffb6dc89cd092d
New-Bucket <azizjonnormatov437@gmail.com>
2022-12-29 09:48:56 +0500

    display

--- src/components/home/comments/CommentDisplay.jsx
+++ src/components/home/comments/CommentDisplay.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import CommentCard from './CommentCard';
-
+//.
 const CommentDisplay = ({ comment, post, replyCm }) => {
     const [showRep, setShowRep] = useState([]);
     const [next, setNext] = useState(1);
